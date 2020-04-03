9525c3cd670b53eee51df61df2a0e0d18d0ce89a
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-03 15:32:24 +0300

    Remove logging

--- app/src/main/java/com/disruption/bakingapp/network/PastryApiServiceProvider.java
+++ app/src/main/java/com/disruption/bakingapp/network/PastryApiServiceProvider.java
@@ -1,7 +1,5 @@
 package com.disruption.bakingapp.network;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,20 +9,12 @@ import static com.disruption.bakingapp.utils.Constants.BASE_URL;
 public class PastryApiServiceProvider {
     private static Retrofit retrofit;
 
-    private static HttpLoggingInterceptor sHttpLoggingInterceptor =
-            new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC);
-
-    private static OkHttpClient sOkHttpClient = new OkHttpClient.Builder()
-            .addInterceptor(sHttpLoggingInterceptor)
-            .build();
-
     private static Retrofit retrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .client(sOkHttpClient)
                     .build();
         }
         return retrofit;
